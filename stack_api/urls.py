b96beed86d1e7d1a83710bacd0edcb379dd712f7
Ambi312 <abd.kanybek@gmail.com>
2022-02-10 15:21:42 +0600

    asdddd

---
+++ stack_api/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from main.views import ProblemViewSet, ReplyViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register('problems', ProblemViewSet)
+router.register('replies', ReplyViewSet)
+router.register('comments', CommentViewSet)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('v1/api/', include(router.urls)),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
